1ca3d73a0436aa5e16961d4286055e9a735f6751
arielg <Yelush0207>
2018-11-14 10:20:20 +0200

    4

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
         System.out.println("hello world my name is navi!!");
         System.out.println("hello world my name is navi2!!");
         System.out.println("hello world my name is navi3!!");
-        System.out.println("hello world my name is navi4!!");
+        System.out.println("hello world my name is navi444444444444444444444444!!");
         System.out.println("hello world my name is branch1/2!!");
         System.out.println("hello world my name is branch24!!");
         System.out.println("hello world my name is master!!");
