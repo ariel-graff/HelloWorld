7a5a9f65a42ced6e4dfcdff1f2a9173ec7e17d51
arielg <Yelush0207>
2018-11-14 09:25:44 +0200

    navi2

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("hello world my name is ariel!!");
         System.out.println("hello world my name is chia!!");
         System.out.println("hello world my name is navi!!");
+        System.out.println("hello world my name is navi2!!");
     }
 }
