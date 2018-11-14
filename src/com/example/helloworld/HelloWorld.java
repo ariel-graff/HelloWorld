da87ec959221e0c7494c304dad7825763ee1af40
arielg <Yelush0207>
2018-11-14 09:20:21 +0200

    ariel

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -3,5 +3,6 @@ package com.example.helloworld;
 public class HelloWorld {
     public static void main(String [] args){
         System.out.println("hello world my name is yelush!!");
+        System.out.println("hello world my name is ariel!!");
     }
 }
